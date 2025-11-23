2f284b944cd17a9f6168a3fd2ad9a1f760e0209f
Yodaman07 <ayaan.irshad@gmail.com>
2025-11-22 22:08:50 -0800

    Session commit 11/22

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,86 @@
+// Based upon 2021's Competition Season DriveTrain code
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+//The DriveTrain is a physical part of the robot. The subsystem is translating part of the robot into a class
+public class DriveTrain extends SubsystemBase 
+{
+  private final WPI_TalonSRX leftDriveTalon;
+  private final WPI_TalonSRX rightDriveTalon;
+  //Initializes the talons
+  //Initializes the nav system
+  private ShuffleboardTab DTTab = Shuffleboard.getTab("DriveTrain");
+  //Initializes the Shuffleboard which is basically the display system. It allows for you to view various robot stats
+  private GenericEntry LeftVoltage = DTTab.add("Left Voltage", 0.0).getEntry();
+  private GenericEntry RightVoltage = DTTab.add("Right Voltage", 0.0).getEntry();
+
+  /** Creates a new DriveTrain */
+  //Constructor
+  public DriveTrain() 
+  {
+    leftDriveTalon = new WPI_TalonSRX(1);
+    rightDriveTalon = new WPI_TalonSRX(0);
+  
+    leftDriveTalon.setNeutralMode(NeutralMode.Coast);
+    rightDriveTalon.setNeutralMode(NeutralMode.Coast);
+
+    leftDriveTalon.setInverted(true);
+    rightDriveTalon.setInverted(false);
+
+    leftDriveTalon.setSensorPhase(true);
+    rightDriveTalon.setSensorPhase(true);
+
+    leftDriveTalon.configFactoryDefault();
+    leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    rightDriveTalon.configFactoryDefault();
+    rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    rightDriveTalon.set(rightSpeed);
+    leftDriveTalon.set(leftSpeed);
+    //Sets the talon power
+  }
+
+  public void resetEncoders() {
+    leftDriveTalon.setSelectedSensorPosition(0,0,10);
+    rightDriveTalon.setSelectedSensorPosition(0,0,10);
+  }
+
+  public double getTicks() {
+    return (leftDriveTalon.getSelectedSensorPosition(0) + rightDriveTalon.getSelectedSensorPosition(0)) / 2.0;
+  }
+
+ 
+
+
+  @Override
+  public void periodic() {
+    //Displays data onto the Shuffleboard (SmartDashboard is outdated). Runs every 2ms
+    SmartDashboard.putNumber("Left Voltage", leftDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Right Voltage", rightDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Myamazingcontributionfortoday",100);
+    SmartDashboard.putNumber("Chakrisfavoritenumber",67);
+    SmartDashboard.putBoolean("IsSoftwareTheBest", true);
+
+    LeftVoltage.setDouble(leftDriveTalon.getMotorOutputPercent());
+    RightVoltage.setDouble(rightDriveTalon.getMotorOutputPercent());
+  }
+}
